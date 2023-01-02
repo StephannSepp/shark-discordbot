3f7a05675b237c23e429bc6ec6bc0bfdd2d4e671
a87146423 <51416119+a87146423@users.noreply.github.com>
2023-01-02 10:59:34 +0800

    Remove flask web

--- bot/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-exec python3 ./bot.py &
-exec gunicorn --bind 0.0.0.0:8000 web:app
--- bot/web.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-
-
-app = Flask(__name__)
-
-
-@app.route("/")
-def home():
-    return "<center><h1>Hellow</h1></center>"
