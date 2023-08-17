15a7cf49bb71715b1a4357c8c71ae0f7c99cbe97
a87146423 <51416119+a87146423@users.noreply.github.com>
2023-08-17 20:06:27 +0800

    version

--- bot/bot.py
+++ bot/bot.py
@@ -16,7 +16,7 @@ from psycopg2.extensions import cursor
 from config import Config
 from utils.time_process import strftimedelta
 
-__version__ = "2.2.571"
+__version__ = "2.3.613"
 
 
 def init_db():
