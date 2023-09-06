b0d1a2be6bf52e49d12716419eba2611657e3026
a87146423 <51416119+a87146423@users.noreply.github.com>
2023-09-06 08:52:52 +0800

    Change algorithm of custom base64

--- bot/utils/funcs.py
+++ bot/utils/funcs.py
@@ -5,7 +5,8 @@ VALID_CHAR = "_AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz-0123456789"
 
 def b64sf_encode(snowflake_id: int):
     """Give a snowflake int and encode it into a modified base64 method."""
-    bsfid = format(snowflake_id, "b").zfill(64)
+    bsfid = format(snowflake_id, "b")
+    bsfid = bsfid.zfill(6 - len(bsfid) % 6 + len(bsfid))
     group = wrap(bsfid, 6)
     return "".join([VALID_CHAR[int(i, 2)] for i in group])
 
@@ -13,5 +14,4 @@ def b64sf_encode(snowflake_id: int):
 def b64sf_decode(encoded_char: str):
     """Give a encoded snowflake string and decode it back to a snowflake ID."""
     decoded = [format(VALID_CHAR.index(c), "b").zfill(6) for c in encoded_char]
-    decoded[-1] = decoded[-1][-4:]
     return int("".join(decoded), 2)
