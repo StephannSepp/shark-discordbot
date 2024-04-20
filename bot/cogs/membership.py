74f7572674d7d1626cd7296493d90a7a5a3e1cf4
a87146423 <51416119+StephannSepp@users.noreply.github.com>
2024-04-20 22:23:41 +0800

    adjust membership assist

--- bot/cogs/membership.py
+++ bot/cogs/membership.py
@@ -56,28 +56,31 @@ class MembershipModal(Modal):
                 "reviewer_uid = %(reviewer_uid)s"
             )
             await cursor.execute(query, params)
-        await self.message.author.add_roles(self.role)
-        try:
-            description = (
-                "[CN] 您的會員證明已被驗證，現在可以使用以下會限頻道了！\n"
-                "[EN] Your proof have been verified and you can now access "
-                "to the following membership channel.\n"
-                "------------------------------------------------------\n"
-                "<#803473713369710653>\n"
-                "<#851664375319494676>\n"
-            )
-            embed = embed_builder.information("會員審核結果通知", description)
-            embed.add_field(
-                "下次帳單日期/Next billing date",
-                next_billing_date.strftime("%Y-%m-%d"),
-                inline=False,
-            )
-            embed.add_field("審核人/Reviewer", f"<@{inter.author.id}>")
-            await self.message.author.send(embed=embed)
-        except Exception:
-            status = "未通知審核結果(未開啟DM)"
+        if self.role not in self.message.author.roles:
+            await self.message.author.add_roles(self.role)
+            try:
+                description = (
+                    "[CN] 您的會員證明已被驗證，現在可以使用以下會限頻道了！\n"
+                    "[EN] Your proof have been verified and you can now access "
+                    "to the following membership channel.\n"
+                    "------------------------------------------------------\n"
+                    "<#803473713369710653>\n"
+                    "<#851664375319494676>\n"
+                )
+                embed = embed_builder.information("會員審核結果通知", description)
+                embed.add_field(
+                    "下次帳單日期/Next billing date",
+                    next_billing_date.strftime("%Y-%m-%d"),
+                    inline=False,
+                )
+                embed.add_field("審核人/Reviewer", f"<@{inter.author.id}>")
+                await self.message.author.send(embed=embed)
+            except Exception:
+                status = "未通知審核結果(未開啟私訊)"
+            else:
+                status = "已通知審核結果(新增身分組)"
         else:
-            status = "已通知審核結果"
+            status = "未通知審核結果(已有身分組)"
         embed = embed_builder.information("更新會員完成")
         embed.add_field("會員", f"<@{self.message.author.id}>", inline=False)
         embed.add_field("下次結帳日期", f"{next_billing_date:%Y-%m-%d}", inline=False)
@@ -90,43 +93,69 @@ class Membership(commands.Cog):
         self.bot = bot
         self.taskloop.start()
 
-    async def check_membership(self) -> None:
-        async with get_cursor() as cursor:
-            query = (
-                "DELETE FROM public.membership "
-                "WHERE next_bill_date <= CURRENT_DATE - INTERVAL '7 days'"
-            )
-            await cursor.execute(query)
-            query = (
-                "SELECT member_uid FROM public.membership "
-                "WHERE next_bill_date = CURRENT_DATE - INTERVAL '3 days' "
-                "AND role_assigned = true"
-            )
-            await cursor.execute(query)
-            result = await cursor.fetchall()
-        if not result:
-            return
-        notif_list = []
-        for row in result:
-            member_uid = row[0]
-            role = self.bot.main_guild.get_role(846616775148044318)
-            member = await self.bot.main_guild.getch_member(member_uid)
-            if member is None:
-                continue
-            await member.remove_roles(role)
-            notif_list.append(f"<@{member_uid}>")
+    async def revoke_roles(self, member_uids: list) -> None:
+        role = self.bot.main_guild.get_role(846616775148044318)
+        for member_uid in member_uids:
             async with get_cursor() as cursor:
                 query = (
                     "UPDATE public.membership SET role_assigned = false "
                     "WHERE member_uid = %(member_uid)s"
                 )
                 await cursor.execute(query, {"member_uid": member_uid})
-        notif = "\n".join(notif_list)
+            member = await self.bot.main_guild.getch_member(member_uid)
+            if member is None:
+                continue
+            if role not in member.roles:
+                continue
+            await member.remove_roles(role)
+
+    async def notify_members(self, member_uids: list) -> str:
+        notif_list = []
+        for member_uid in member_uids:
+            member = await self.bot.main_guild.getch_member(member_uid)
+            if member is None:
+                continue
+            notif_list.append(f"<@{member_uid}>")
+        return "\n".join(notif_list)
+
+    async def check_membership(self) -> None:
+        async with get_cursor() as cursor:
+            query = (
+                "SELECT member_uid FROM public.membership "
+                "WHERE next_bill_date = CURRENT_DATE - INTERVAL '2 days' "
+                "AND role_assigned = true"
+            )
+            await cursor.execute(query)
+            to_notify = await cursor.fetchall()
+            query = (
+                "SELECT member_uid FROM public.membership "
+                "WHERE next_bill_date <= CURRENT_DATE - INTERVAL '3 days' "
+                "AND role_assigned = true"
+            )
+            await cursor.execute(query)
+            to_revoke = await cursor.fetchall()
+            query = (
+                "DELETE FROM public.membership "
+                "WHERE next_bill_date <= CURRENT_DATE - INTERVAL '28 days'"
+                "AND role_assigned = false"
+            )
+            await cursor.execute(query)
+        await self.revoke_roles([row[0] for row in to_revoke])
+        if not to_notify:
+            return
+        notif = await self.notify_members([row[0] for row in to_notify])
         channel = self.bot.get_channel(847459494253690930)
         await channel.send(
             "以下蝦蝦們請於 <#846613455351185429> 重新提交會員證明\n"
             f"若已使用自動審核請無視這次通知\n{notif}"
         )
+
+    @commands.slash_command(name="manual_check_membership")
+    @commands.default_member_permissions(administrator=True)
+    async def manual_check_membership(self, inter: CmdInter) -> None:
+        await inter.response.defer()
+        await self.check_membership()
+        await inter.edit_original_response("Done")
 
     @commands.message_command(name="membership")
     @commands.default_member_permissions(moderate_members=True)
