e4ae5f268206f999e9157f1161bd2a5163d06bbe
a87146423 <51416119+StephannSepp@users.noreply.github.com>
2024-05-03 23:22:23 +0800

    Modify(checks): Restrict the command channel check only affect in the home guild

--- bot/utils/checks.py
+++ bot/utils/checks.py
@@ -1,8 +1,10 @@
+from config import Config
 from disnake import CmdInter
 from disnake.ext.commands.errors import CheckFailure
 
 
 def is_on_command_channel(inter: CmdInter):
-    if inter.channel_id != 761814788589223978:
-        raise CheckFailure("該指令僅能用於<#761814788589223978>中")
+    if inter.guild_id == Config.home_guild:
+        if inter.channel_id != 761814788589223978:
+            raise CheckFailure("該指令僅能用於<#761814788589223978>中")
     return True
